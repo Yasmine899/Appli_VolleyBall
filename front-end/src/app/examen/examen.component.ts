92c2c53857479ca3b0554bbc5e3b32076d419c93
EmilieGreaker1 <emigre01@hotmail.com>
2024-04-03 12:08:18 +0200

    examen component beginning

---
+++ front-end/src/app/examen/examen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  standalone: true,
+  imports: [],
+  templateUrl: './examen.component.html',
+  styleUrl: './examen.component.scss'
+})
+export class ExamenComponent {
+
+}
