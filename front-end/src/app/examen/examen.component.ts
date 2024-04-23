7de1fa55b29a0fb3b59ee3e99c8fc33352522d88
EmilieGreaker1 <emigre01@hotmail.com>
2024-04-23 09:02:38 +0200

    small name fixes

--- front-end/src/app/examen/examen.component.ts
+++ front-end/src/app/examen/examen.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-quiz',
+  selector: 'app-examen',
   standalone: true,
   imports: [],
   templateUrl: './examen.component.html',
   styleUrl: './examen.component.scss'
 })
 export class ExamenComponent {
-
+  
 }
