51e04ae9c61097c0aae015babe510bb621758367
EmilieGreaker1 <emigre01@hotmail.com>
2024-04-26 12:13:25 +0200

    exam

--- front-end/src/app/examen/examen.component.ts
+++ front-end/src/app/examen/examen.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuestionService } from '../question/question.service';
+import { Question } from '../question/question';
 
 @Component({
   selector: 'app-examen',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './examen.component.html',
   styleUrl: './examen.component.scss'
 })
-export class ExamenComponent {
-  
+export class ExamenComponent implements AfterViewInit {
+  @ViewChild('exam_preview') exam_preview: ElementRef | undefined;
+  public questions: Question[] = [];
+
+  constructor(private questionService: QuestionService) { }
+
+  ngAfterViewInit() {
+    if (this.exam_preview) {
+      console.log(this.exam_preview.nativeElement.innerHTML);
+    }
+  }
+
+  ngOnInit(): void {
+    this.questions = this.questionService.getQuestions();
+  }
+
+  print_exam() {
+    if (this.exam_preview) {
+      this.exam_preview.nativeElement.innerHTML.print();
+    }
+  }
 }
