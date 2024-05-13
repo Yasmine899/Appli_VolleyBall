cd6ea5b2b6f682e06de781116c8ae3a7a0df0967
EmilieGreaker1 <emigre01@hotmail.com>
2024-05-13 15:48:42 +0200

    Assistance and examen
    Simple Assistance component added
    Examen format and printing of examen reworked and formatted

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ExamenComponent } from "./examen/examen.component";
 import { CoursComponent } from "./cours/cours.component";
+import { AssistanceComponent } from "./assistance/assistance.component";
 import { AppRoutingModule } from './app.routes';
 
 @Component({
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app.routes';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, RouterModule, HeaderComponent, ExamenComponent, CoursComponent]
+    imports: [RouterOutlet, RouterModule, HeaderComponent, ExamenComponent, CoursComponent, AssistanceComponent]
 })
 export class AppComponent {
   title = 'front-end';
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { CoursComponent } from './cours/cours.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { EntrainementComponent } from './entrainement/entrainement.component';
 import { ExamenComponent } from './examen/examen.component';
+import { AssistanceComponent } from "./assistance/assistance.component";
 
 export const routes: Routes = [
   // Other routes...
   { path: 'cours', component: CoursComponent },
   { path: 'entrainement', component: EntrainementComponent},
   { path: 'examen', component: ExamenComponent},
+  { path: 'assistance', component: AssistanceComponent},
   { path: '', component: LandingPageComponent}
 ];
 
