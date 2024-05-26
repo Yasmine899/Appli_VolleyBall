8c3f9499fbb1b3fc552be668e2fe01e9a3b190c0
Kelian S <kelian.sbc@gmail.com>
2024-05-26 14:54:31 +0200

    Section title api added

--- backend_volleyball/src/main/java/backend/Controller/CourseController.java
+++ backend_volleyball/src/main/java/backend/Controller/CourseController.java
@@ -1,11 +1,14 @@
 package backend.Controller;
 
 import backend.Model.Course;
+import backend.Model.Section;
 import backend.Service.CourseService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -21,5 +24,10 @@ public class CourseController {
     @RequestMapping("/chapter/section/{sectionId}")
     public List<Integer> getChapitreBySection(@PathVariable int sectionId){
         return CourseService.getChapitreBySection(sectionId);
+    }
+
+    @RequestMapping("/sections")
+    public ArrayList<Section> getAllSections(){
+        return CourseService.getAllSections();
     }
 }
