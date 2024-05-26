e32ecb83846ec1a49bc05b679e88b70d1b0ca0a1
DanAun <daniel.aunan@gmail.com>
2024-05-26 18:19:56 +0200

    Added table of contents to cours-section

--- front-end/src/app/cours-section/cours-section.component.ts
+++ front-end/src/app/cours-section/cours-section.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ChapitreComponent } from '../chapitre/chapitre.component';
 import {CoursApiConnectService} from '../cours-api-connect.service';
 
 @Component({
   selector: 'app-cours-section',
   standalone: true,
-  imports: [CommonModule, ChapitreComponent],
+  imports: [CommonModule, ChapitreComponent, RouterModule],
   templateUrl: './cours-section.component.html',
   styleUrl: './cours-section.component.scss',
   providers: [CoursApiConnectService]
@@ -17,6 +18,7 @@ export class CoursSectionComponent implements OnInit{
   // The section title and id
   sectionTitle: string;
   sectionId: number;
+  showTableofContents: boolean = false;
   containingChapters: number[];
 
 
@@ -43,6 +45,14 @@ export class CoursSectionComponent implements OnInit{
       // Gets the list of chapters for this section
       this.coursApiConnectService.getSectionChapters(this.sectionId).subscribe((data : any) => {
         this.containingChapters = data;
+
+        // If there are chapters in this section between 1 and 98, show the table of contents
+        for (let i = 0; i < this.containingChapters.length; i++) {
+          if (this.containingChapters[i] > 0 && this.containingChapters[i] < 99) {
+            this.showTableofContents = true;
+            break;
+          }
+        }
       });
 
     });
